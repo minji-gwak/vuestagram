1a07f6d0bcafaaba67f7fb11318f242a07becce0
곽민지 <mini0006@paiptree.com>
2022-03-14 14:57:54 +0900

    1강

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Post v-for="(post, i) in post_data" :key="i" :post=post_data[i] />
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    props: [
+        'post_data'
+    ],
+    components: {
+        Post
+    }
+}
+</script>
